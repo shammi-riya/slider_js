f81bc19e420bd83e0cef2033884d9eea16f22477
shammi-Riya~ <shammiriya455@gmail.com>
2023-03-16 11:03:09 -0700

    some chnge and last commit

--- js/slider.js
+++ js/slider.js
@@ -36,32 +36,4 @@ const prevArrow = () =>{
 
 
 
-// const changeImg = document.getElementById("imge")
 
-// let count = 0
-// const nextArrow=()=>{  
-//   if(count >= sliderImg.length-1 ){
-//      count = 0
-//      changeImg.src= sliderImg[count]
-//   }else{
-//     count++
-//     changeImg.src= sliderImg[count]
-   
-//   }   
-// }
-
-
-// const prevArrow = ()=>{
-  
-//   console.log(count);
-  
-//   if(count <=1){
-//     const imgIndex = sliderImg[count]
-//     count = sliderImg.length-1
-//     changeImg.src= imgIndex
-//   }else{
-//     count--
-//     const imgIndex = sliderImg[count]
-//     changeImg.src= imgIndex
-//   }
-// }
